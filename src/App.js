e8e01b248929a23064a602f0af3f1299bb932e6c
Jamie Maw <84570007+jamiemaw-cse@users.noreply.github.com>
2022-09-18 13:11:03 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SourceInput from './SourceInput';
+
+function App() {
+  return (
+    <div className='App'>
+      <SourceInput />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/SourceInput.js
@@ -0,0 +1,41 @@
+/* global chrome */
+import React from 'react'
+import './App.css'
+
+function SourceInput() {
+
+    function search() {
+        let source = document.getElementById('sourceInput').value
+        console.log(source.length)
+
+        if (/^0x[a-fA-F0-9]{40}$/.test(source)){
+            // Ethereum address
+            chrome.tabs.create({active: true, url: 'https://blockscan.com/address/' + source})
+        }
+        else if (/^0x([A-Fa-f0-9]{64})$/.test(source)){
+            // Ethereum transaction
+            chrome.tabs.create({active: true, url: 'https://blockscan.com/tx/' + source})
+        }
+        else if (/^r[1-9A-HJ-NP-Za-km-z]{25,33}$/.test(source)){
+            // XRP address
+            chrome.tabs.create({active: true, url: 'https://xrpscan.com/account/' + source})
+        }
+        else if (/^[A-F0-9]{64}$/.test(source)){
+            // XRP transaction
+            chrome.tabs.create({active: true, url: 'https://xrpscan.com/tx/' + source})
+        }
+        else {
+            console.log('Not a valid address or transaction ID')
+        }
+    }
+
+  return (
+    <div className='sourceInputContainer'>
+        <h2>Support Explorer</h2>
+        <input id='sourceInput' className='sourceInput' placeholder='Address/Transaction ID'></input>
+        <button onClick={search}>Search</button>
+    </div>
+  )
+}
+
+export default SourceInput
