fd199ea5515b64fd01a245e50d9435b13c686177
Scott Lozano <lozans89@gmail.com>
2022-10-04 14:58:55 +0100

    updates icon size and begins dictionary

--- src/components/SourceInput.js
+++ src/components/SourceInput.js
@@ -10,6 +10,8 @@ import btc from './btc.svg';
 
 function SourceInput() {
 
+    //const SEARCH_URL = 'https://blockchair.com/search?q=';
+
     const [showResults, setShowResults] = useState(); //@dev Used to hide or show results in popup. Inactive until search activated. 
     const [imgSrc, setImgSrc] = useState(); //@dev Used to set the URL for the iframe depending on regex match. Where to search the transaction hash. 
     const [imgUrl, setImgUrl] = useState();
@@ -31,6 +33,45 @@ function SourceInput() {
         setTimeout(function() { snackbar.className = snackbar.className.replace("show", ""); }, 1800);
     }
 
+
+//@dev This will be the function to loop the dictionary and take the source variable as the hash. 
+    async function hashDictionary(hash) {
+     let chain = false;
+     let hashRegex;
+     let coinIcon;
+
+     //@dev Dictionary for tx regexes, search parameters, chain and can add addresses.
+
+     let hashRegexs = {
+         "bitcoin": "^[0-9a-f]{64}$:btc",
+         "ethereum": "^0x[0-9a-fA-F]{64}$:eth",
+         "tezos": "^o[a-zA-Z0-9]{50}:tezos",
+         "cardano":"9A-HJ-NP-Za-km-z]+:ada",
+         "cardano":"addr1[a-z0-9]+:ada",
+         "monero":"4[0-9AB][1-9A-HJ-NP-Za-km-z]{93}$:xmr",
+         "stellar":"^G[A-Z0-9]{55}$:xlm",
+         "ripple":"r[rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz]{27,35}$:xrp",
+
+     }
+
+     for (let keys in hashRegexs) {
+         //@dev keys are the chains
+         hashRegex = hashRegexs[keys].split(':')[0]; //@dev Get regex
+
+         let regex = new RegExp(hashRegex, 'gi'); //@dev formats as regex.
+
+         if (regex.test(hash)) {
+             chain = keys;
+             coinIcon = hashRegexs[keys].split(':')[1]; //@dev Get coin icon
+             console.log("Coin icon is for: ", chain);
+         } else {
+             console.log("Not the right chain", keys);
+         }
+     };
+
+ }
+
+
     const search = async () => {
         let source = document.getElementById('sourceInput').value
         console.log(source.length)
@@ -39,11 +80,11 @@ function SourceInput() {
             var x = await document.getElementById("snackbar");
             x.innerText = `${getRandomEmoji()}`;
             x.className = "show";
-            setTimeout(function() { x.className = x.className.replace("show", ""); }, 1800);
+            setTimeout(function() { x.className = x.className.replace("show", ""); }, 1100);
         } else if (/^0x[a-fA-F0-9]{40}$/.test(source)) {
             // Ethereum address
             //chrome.tabs.create({active: true, url: 'https://blockscan.com/address/' + source})
-            let address = 'https://blockscan.com/address/' + source;
+            let address = 'https://blockchair.com/search?q=' + source;
             console.log("setting Eth add.")
             await setImgSrc(eth);
             await setImgUrl(address);
@@ -61,7 +102,7 @@ function SourceInput() {
         } else if (/^r[1-9A-HJ-NP-Za-km-z]{25,33}$/.test(source)) {
             // XRP address
             //chrome.tabs.create({active: true, url: 'https://xrpscan.com/account/' + source})
-            let address = 'https://xrpscan.com/account/' + source;
+            let address = 'https://blockchair.com/search?q=' + source;
             await setImgUrl(address);
             await setImgSrc(xrp);
             foundText();
@@ -94,7 +135,7 @@ function SourceInput() {
 
         {showResults && <div> 
         <br/>
-        <a href={imgUrl} onClick={link} title="Chain icons"> <img class="animated bounce" style={{ width: "auto" , height:"2em" }} src={imgSrc} /> </a> </div> }
+        <a href={imgUrl} onClick={link} title="Chain icon(s)"> <img class="animated bounce" style={{ width: "auto" , height:"4em", marginBottom:"10px" }} src={imgSrc} /> </a> </div> }
         
 
         <div id="snackbar"></div>
