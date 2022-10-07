efeabc0a1bfb136ab59a0a363a11a574adc088f4
Scott Lozano <lozans89@gmail.com>
2022-10-07 19:46:49 +0100

    updates component structure for existing tab check and Source Input

--- src/components/SourceInput.js
+++ src/components/SourceInput.js
@@ -6,6 +6,7 @@ import spinner from './spinner-solid.svg';
 import searchIcon from './magnifying-glass-solid.svg';
 import validateSolAddress from './solana.js';
 import validateSignature from './solSignature.js';
+import existingTabCheck from './existingTab.js';
 //import getAlgoData from './algoTx.js';
 //import verifyTezTx from './tezTx.js';
 
@@ -17,58 +18,6 @@ function SourceInput() {
     const getRandomEmoji = () => {
         return emojis[~~(Math.random() * emojis.length)]
     };
-
-//@dev Checks for tab open matching the URL or source (as part of the URL). Redirects/focuses on to that tab if opened, regardless of window.
-    const existingTabCheck = async (URL, source) => {
-       var found = false;
-       var tabId;
-       let focusedTab;
-       let currentTab;
-
-       try {
-          await chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-               currentTab = tabs[0];
-               if (currentTab.url.search(URL) > -1 || currentTab.url.search(source) > -1) {
-                   focusedTab = currentTab;
-               }
-           });
-
-          await chrome.windows.getAll({ populate: true }, function(windows) {
-               windows.forEach(function(window) {
-                   window.tabs.forEach(function(tab) {
-                       if (tab.url.search(URL) > -1 || tab.url.search(source) > -1) {
-                           found = true;
-                           tabId = tab.id;
-                       }
-                   })
-               })
-               if (found == false) {
-                   chrome.tabs.create({ active: true, url: URL + source });
-               } else if (focusedTab == currentTab) {
-                   var snackbar = document.getElementById("snackbar");
-                   snackbar.innerText = "You're currently there ✅";
-                   snackbar.className = "show";
-                   snackbar.style.right = "50%";
-                    setTimeout(function() { 
-                        snackbar.className = snackbar.className.replace("show", ""); 
-                        snackbar.style.right = snackbar.style.right.replace("50%", "60%")
-                    }, 1500);
-               } else {
-                   chrome.tabs.update(tabId, { selected: true });
-                   var snackbar = document.getElementById("snackbar");
-                   snackbar.innerText = 'The explorer is now open in your other window 👀';
-                   snackbar.className = "show";
-                   snackbar.style.right = "20%";
-                   setTimeout(function() { 
-                        snackbar.className = snackbar.className.replace("show", ""); 
-                        snackbar.style.right = snackbar.style.right.replace("20%", "60%")
-                    }, 2500);
-               }
-           });
-       } catch (e) {
-           console.log(e);
-       }
-   }
 
    const lookUpText = async () => {
      var snackbar = await document.getElementById("snackbar");
---
+++ src/components/existingWindow.js
@@ -0,0 +1,56 @@
+// //@dev Checks for window open matching the URL or source (as part of the URL). Redirects/focuses on to that window if opened.
+// NOT YET ADAPTED FOR WINDOWS.
+
+    
+// const existingWindowCheck = async (URL, source) => {
+//        var found = false;
+//        var tabId;
+//        let focusedWindow;
+//        let currentWindow;
+
+//        try {
+//           await chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+//                currentWindow = tabs[0];
+//                if (currentTab.url.search(URL) > -1 || currentTab.url.search(source) > -1) {
+//                    focusedWindow = currentWindow;
+//                }
+//            });
+
+//           await chrome.windows.getAll({ populate: true }, function(windows) {
+//                windows.forEach(function(window) {
+//                    window.tabs.forEach(function(tab) {
+//                        if (tab.url.search(URL) > -1 || tab.url.search(source) > -1) {
+//                            found = true;
+//                            tabId = tab.id;
+//                        }
+//                    })
+//                })
+//                if (found == false) {
+//                    chrome.tabs.create({ active: true, url: URL + source });
+//                } else if (focusedTab == currentTab) {
+//                    var snackbar = document.getElementById("snackbar");
+//                    snackbar.innerText = "This window has your explorer ✅";
+//                    snackbar.className = "show";
+//                    snackbar.style.right = "40%";
+//                     setTimeout(function() { 
+//                         snackbar.className = snackbar.className.replace("show", ""); 
+//                         snackbar.style.right = snackbar.style.right.replace("40%", "60%")
+//                     }, 1500);
+//                } else {
+//                    chrome.tabs.update(tabId, { selected: true });
+//                    var snackbar = document.getElementById("snackbar");
+//                    snackbar.innerText = 'The explorer is now open in your other window 👀';
+//                    snackbar.className = "show";
+//                    snackbar.style.right = "20%";
+//                    setTimeout(function() { 
+//                         snackbar.className = snackbar.className.replace("show", ""); 
+//                         snackbar.style.right = snackbar.style.right.replace("20%", "60%")
+//                     }, 2500);
+//                }
+//            });
+//        } catch (e) {
+//            console.log(e);
+//        }
+//    }
+
+//    export default existingWindowCheck;
